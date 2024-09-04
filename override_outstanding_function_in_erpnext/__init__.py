409453dad3fdf6645bc7bbf56453b8c548e7d32e
Ahmed Mostafa <66416615+citybirdman@users.noreply.github.com>
2024-09-04 13:31:36 +0300

    Update __init__.py

--- override_outstanding_function_in_erpnext/__init__.py
+++ override_outstanding_function_in_erpnext/__init__.py
@@ -1,1 +1,5 @@
-__version__ = "1.0.1"
+_version__ = "1.0.1"
+import erpnext
+#import override_outstanding_function_in_erpnext
+from override_outstanding_function_in_erpnext.customer import get_customer_outstanding
+erpnext.selling.doctype.customer.customer.get_customer_outstanding = get_customer_outstanding
