ab533bdaa2cd8841efdf5de0ca28f59b86745a85
Ahmed Mostafa <66416615+citybirdman@users.noreply.github.com>
2024-09-04 14:50:21 +0300

    Update hooks.py

--- override_outstanding_function_in_erpnext/hooks.py
+++ override_outstanding_function_in_erpnext/hooks.py
@@ -109,7 +109,7 @@ app_license = "MIT"
 # Override standard doctype classes
 
 override_doctype_class = {
-	"Customer": "override_outstanding_function_in_erpnext.customer"
+	# "Customer": "override_outstanding_function_in_erpnext.customer"
 }
 
 # Document Events
