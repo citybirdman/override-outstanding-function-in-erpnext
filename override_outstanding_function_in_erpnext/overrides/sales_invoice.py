41b2620265a28dd94b9182dd2883c04afefcbb59
citybirdman <your-email@domain.com>
2025-12-22 15:17:14 +0000

    Fix override

--- override_outstanding_function_in_erpnext/overrides/sales_invoice.py
+++ override_outstanding_function_in_erpnext/overrides/sales_invoice.py
@@ -1,5 +1,8 @@
-from erpnext.accounts.doctype.sales_invoice.sales_invoice import SalesInvoice
-
+from stock_delivered_unbilled.stock_delivered_unbilled.overrides.sales_invoice import CustomSalesInvoice as SalesInvoice
+# import frappe
+# from frappe.model.base_document import get_controller
+# class_controller = get_controller("Sales Invoice")
+# frappe.get
 class CustomSalesInvoice(SalesInvoice):
     def check_sales_order_on_hold_or_close(self, ref_fieldname):
         pass
